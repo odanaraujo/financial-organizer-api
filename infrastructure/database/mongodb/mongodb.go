6059a12a1bebe2bd0a087b5d2d0b71382aefe27f
odanaraujo <dansa.recteck@gmail.com>
2024-05-30 23:00:46 -0300

    feat: connection mongodb

---
+++ infrastructure/database/mongodb/mongodb.go
@@ -0,0 +1,29 @@
+package mongodb
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
+)
+
+const (
+	MONGODB_URL     = "MONGODB_URL"
+	MONGODB_USER_DB = "MONGODB_USER_DB"
+)
+
+func NewMongoDBConnection() (*mongo.Database, error) {
+	mongoURI := os.Getenv(MONGODB_URL)
+	mongoUser := os.Getenv(MONGODB_USER_DB)
+
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+	return client.Database(mongoUser), nil
+}
