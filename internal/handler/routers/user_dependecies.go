cb13521f615ee9227f203c90fc756610c4260d50
odanaraujo <dansa.recteck@gmail.com>
2024-05-30 23:00:09 -0300

    feat: dependencies and refactor mongo.database

--- internal/handler/routers/user_dependecies.go
+++ internal/handler/routers/user_dependecies.go
@@ -1,19 +1,16 @@
 package routers
 
 import (
-	"database/sql"
-	"github.com/gin-gonic/gin"
 	userHandler "github.com/odanraujo/financial-organizer-api/internal/handler/users"
 	"github.com/odanraujo/financial-organizer-api/internal/repository/users"
 	usecase "github.com/odanraujo/financial-organizer-api/internal/usecase/users"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func NewRouter(db *sql.DB) *gin.Engine {
-	userRepo := users.NewUserRepository(db)
+func UserInitDependecies(mongoDB *mongo.Database) userHandler.UserHandler {
+	userRepo := users.NewUserRepository(mongoDB)
 	userUsercase := usecase.NewUserUsecase(userRepo)
 	userController := userHandler.NewUserHandler(userUsercase)
 
-	r := gin.Default()
-	r.POST("/routers", userController.CreateUser)
-	return r
+	return userController
 }
